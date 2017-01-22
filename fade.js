dbeb12f74d32c25f55f96aa433b9320355d4df45
Vinay Shanbhag <2015shanbhvi@gmail.com>
2017-01-22 03:19:04 -0500

    gonna stick with one page

---
+++ fade.js
@@ -0,0 +1,19 @@
+var el = document.getElementById("fade_this");
+
+function fadeIn(el) {
+  el.style.opacity = 0;
+
+
+  var tick = function() {
+    el.style.opacity = +el.style.opacity + 0.01;
+
+
+    if (+el.style.opacity < 1) {
+      (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+    }
+  };
+
+  tick();
+}
+
+fadeIn(el);
