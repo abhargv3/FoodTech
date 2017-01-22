976db677ce0018097c4ec77285ffaf203c26f3e7
Aditya Bhargava <abhargv3@illinois.edu>
2017-01-22 02:21:31 -0600

    updated schema

--- heroku_app/server/util/food.js
+++ heroku_app/server/util/food.js
@@ -3,7 +3,8 @@
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var FoodSchema   = new Schema({	
+var FoodSchema   = new Schema({
+	recipe:String,
     text: String,
     quantity: Number,
     measure: String,
